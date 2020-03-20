acab7eea8065a949ccedc8b161d67bca01ee6b5d
mihalichpalich <pavlovtfspb@gmail.com>
2020-03-20 12:18:02 +0300

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/test', function(req, res) {
+    console.log(req.params);
+    return res.send("Hello!");
+});
+
+app.listen(6666, function (err) {
+    if (err) {
+        return console.log(err)
+    }
+    console.log('Server run');
+});
